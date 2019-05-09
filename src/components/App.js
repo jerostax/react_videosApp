3054478c7aec1ef8e449dda2b4d162568a71e47c
jerostax <jeremy.geneste@gmail.com>
2019-05-09 14:21:02 +0200

    [ FEATURE ] Added VideoList component and passed videos as props

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
+import VideoList from './VideoList';
 
 class App extends React.Component {
   state = { videos: [] };
@@ -19,7 +20,8 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        Il y a {this.state.videos.length} vidéos !
+        <VideoList videos={this.state.videos} />
+
       </div>
     );
   }
