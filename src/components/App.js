141d7703fe418c8bbeccfa642e41aa33a95909a9
jerostax <jeremy.geneste@gmail.com>
2019-05-09 12:31:53 +0200

    [ CONFIG ] Modified onTermSubmit as an Async Arrow fx, initialized videos state and setState

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,23 @@ import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
 
 class App extends React.Component {
+  state = { videos: [] };
 
-  onTermSubmit = term => {
-    youtube.get('/search', {
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
+
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        Il y a {this.state.videos.length} vidéos !
       </div>
     );
   }
