88d61f636e72d05b3ba653aa6e930a4ac6df9ac9
jerostax <jeremy.geneste@gmail.com>
2019-05-09 10:14:52 +0200

    [ DESIGN ] Added SearchBar form + Semantic UI classes

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>App Component</h1>
+      <div className="ui container">
         <SearchBar />
       </div>
     );
